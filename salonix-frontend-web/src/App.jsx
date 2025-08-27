6bcb487f032c17f49e0110b0d58648b2c8b3f3c5
NemoIII <pablo.iii@outlook.com>
2025-08-27 16:01:20 +0100

    FEW: 02 - Routes - Configurar rotas com React Router #2

--- salonix-frontend-web/src/App.jsx
+++ salonix-frontend-web/src/App.jsx
@@ -1,8 +1,19 @@
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import Router from './routes/Router';
 import { useTranslation } from 'react-i18next';
 
 function App() {
   const { t } = useTranslation();
-  return <h1>{t('hello')}</h1>;
+
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <h1>{t('hello')}</h1> {/* Temporário para testes */}
+        <Router />
+      </BrowserRouter>
+    </AuthProvider>
+  );
 }
 
 export default App;
